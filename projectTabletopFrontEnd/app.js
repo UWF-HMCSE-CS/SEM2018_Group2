0f48362f140deca3da155ff01162db091ed2ac1e
alc73 <alc73@students.uwf.edu>
2018-10-22 16:40:55 -0500

    App runs on local machine, little design functionality

---
+++ projectTabletopFrontEnd/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+let app = express();
+
+// set up handlebars view engine
+let handlebars = require('express-handlebars')
+	.create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+// Renders the landing page
+app.get('/', function(req, res) {
+	res.render('landingPage');
+});
+
+
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+	res.status(404);
+	res.render('404');
+});
+
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log( 'Express started on http://localhost:' +
+    app.get('port') + '; press Ctrl-C to terminate.' );
+});
