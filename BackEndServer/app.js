9ce4683dc20a1d671e834fe679929c938ac1789f
Omar Garcia <og8@students.uwf.edu>
2018-11-06 18:51:58 -0600

    Refactored code to use post instead of get. The server will recieve a post command and return a list of post that match the reqiurements.

--- BackEndServer/app.js
+++ BackEndServer/app.js
@@ -31,50 +31,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.get('/getMember/:player_id', function(req, res) {
-    value = req.params.player_id;
-    console.log(value);
+app.post('/getMember', function(req, res) {
     Table = "MEMBER";
-    player_id = "" + value;
 
     var params = {
         TableName : Table,
-        Key: {
-            "player_id": player_id,
-        }
-    };
-
-    docClient.get(params, function(err, data) {
-        if (err) {
-            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
-        } else {
-            console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
-            res.send(data);
-        }
-    });
-});
-
-app.post('/getLFM', function(req, res) {
-    console.log("Request: = ");
-    Table = "LFM_POST";
-
-    var params = {
-        TableName : Table,
-        ProjectionExpression: "#gt,#reg,#st,#dp",
+        ProjectionExpression: "#zip, email, first_name, last_name, password, username, player_id",
         FilterExpression:
-            "#gt = :game AND #req = :region AND #st = :session AND #dp = :display",
+            "#zip = :zip",
         ExpressionAttributeNames: {
-            "#gt": "gameType",
-            "#reg" : "region",
-            "#st" : "sessionType",
-            "#dp" : "displayPosts",
+            "#zip": "zip_code",
         },
         ExpressionAttributeValues: {
-            ":game" : query.gameType,
-            ":region" : query.region,
-            ":session" : query.sessionType,
-            ":display" : query.displayPosts,
-
+            ":zip" : req.body.zip_code,
         }
     };
 
@@ -86,11 +55,10 @@ app.post('/getLFM', function(req, res) {
         } else {
             // print all the movies
             console.log("Scan succeeded.");
-            data.Items.forEach(function (movie) {
-                console.log(
-                    movie.year + ": ",
-                    movie.title, "- rating:", movie.info.rating);
+            data.Items.forEach(function (mem) {
+                console.log("player id = " + mem.player_id);
             });
+            res.send(data);
 
             // continue scanning if we have more movies, because
             // scan can retrieve a maximum of 1MB of data
@@ -103,121 +71,219 @@ app.post('/getLFM', function(req, res) {
     }
 });
 
-/*
-app.get('/getLFM/:player_post_id', function(req, res) {
-    console.log("Request: = ");
-    value = req.params.player_post_id;
-    //console.log(value);
-    Table = "LFM_POST";
-    player_post_id = "" + value
 
-    var params = {
-        TableName : Table,
-        Key: {
-            "player_post_id": player_post_id,
-        }
-    };
-
-    docClient.get(params, function(err, data) {
-        if (err) {
-            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
-        } else {
-            //console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
-            res.send(data);
-        }
-    });
-});
-*/
-app.get('/getLFG/:player_post_id', function(req, res) {
-    value = req.params.player_post_id;
-    console.log(value);
+app.post('/getLFG', function(req, res) {
     Table = "LFG_POST";
-    player_post_id = "" + value
 
     var params = {
         TableName : Table,
-        Key: {
-            "player_post_id": player_post_id,
+        ProjectionExpression: "#gt, char_lvl, description, DM, num_players, player_post_id, schedule", //"#gt,#reg,#st,#dp",
+        FilterExpression:
+            "#gt = :game",//"#gt = :game AND #req = :region AND #st = :session AND #dp = :display",
+        ExpressionAttributeNames: {
+            "#gt": "game_type",
+            //"#reg" : "region",
+            //"#st" : "sessionType",
+            //"#dp" : "displayPosts",
+        },
+        ExpressionAttributeValues: {
+            ":game" : req.body.game_type,
+            //":region" : query.region,
+            //":session" : query.sessionType,
+            //":display" : query.displayPosts,
+
         }
     };
 
-    docClient.get(params, function(err, data) {
+    docClient.scan(params, onScan);
+
+    function onScan(err, data) {
         if (err) {
-            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+            console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
         } else {
-            console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+            // print all the movies
+            console.log("Scan succeeded.");
+            data.Items.forEach(function (LFM_POST) {
+                console.log("player id = " + LFM_POST.player_post_id);
+            });
             res.send(data);
+
+            // continue scanning if we have more movies, because
+            // scan can retrieve a maximum of 1MB of data
+            if (typeof data.LastEvaluatedKey != "undefined") {
+                console.log("Scanning for more...");
+                params.ExclusiveStartKey = data.LastEvaluatedKey;
+                docClient.scan(params, onScan);
+            }
         }
-    });
+    }
 });
 
-app.get('/getInvite/:player_id', function(req, res) {
-    value = req.params.player_id;
-    console.log(value);
+app.post('/getLFM', function(req, res) {
+    Table = "LFM_POST";
+
+    var params = {
+        TableName : Table,
+        ProjectionExpression: "#gt, char_lvl, description, DM, num_players, player_post_id, schedule", //"#gt,#reg,#st,#dp",
+        FilterExpression:
+            "#gt = :game",//"#gt = :game AND #req = :region AND #st = :session AND #dp = :display",
+        ExpressionAttributeNames: {
+            "#gt": "game_type",
+            //"#reg" : "region",
+            //"#st" : "sessionType",
+            //"#dp" : "displayPosts",
+        },
+        ExpressionAttributeValues: {
+            ":game" : req.body.game_type,
+            //":region" : query.region,
+            //":session" : query.sessionType,
+            //":display" : query.displayPosts,
+
+        }
+    };
+
+    docClient.scan(params, onScan);
+
+    function onScan(err, data) {
+        if (err) {
+            console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            // print all the movies
+            console.log("Scan succeeded.");
+            data.Items.forEach(function (LFM_POST) {
+                console.log("post id = " + LFM_POST.player_post_id);
+            });
+            res.send(data);
+
+            // continue scanning if we have more movies, because
+            // scan can retrieve a maximum of 1MB of data
+            if (typeof data.LastEvaluatedKey != "undefined") {
+                console.log("Scanning for more...");
+                params.ExclusiveStartKey = data.LastEvaluatedKey;
+                docClient.scan(params, onScan);
+            }
+        }
+    }
+});
+
+app.post('/getInvite', function(req, res) {
     Table = "INVITE";
-    player_id = "" + value
 
     var params = {
         TableName : Table,
-        Key: {
-            "player_id": player_id,
-        }
+        ProjectionExpression: "#id, link, msg", //"#gt,#reg,#st,#dp",
+        FilterExpression:
+            "#id = :player",//"#gt = :game AND #req = :region AND #st = :session AND #dp = :display",
+        ExpressionAttributeNames: {
+            "#id": "player_id",
+        },
+        ExpressionAttributeValues: {
+            ":player" : req.body.player_id,
+ }
     };
 
-    docClient.get(params, function(err, data) {
+    docClient.scan(params, onScan);
+
+    function onScan(err, data) {
         if (err) {
-            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+            console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
         } else {
-            console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+            // print all the movies
+            console.log("Scan succeeded.");
+            data.Items.forEach(function (invite) {
+                console.log("player id = " + invite.player_id);
+            });
             res.send(data);
+
+            // continue scanning if we have more movies, because
+            // scan can retrieve a maximum of 1MB of data
+            if (typeof data.LastEvaluatedKey != "undefined") {
+                console.log("Scanning for more...");
+                params.ExclusiveStartKey = data.LastEvaluatedKey;
+                docClient.scan(params, onScan);
+            }
         }
-    });
+    }
 });
 
-app.get('/getGroup/:group_id', function(req, res) {
-    value = req.params.group_id;
-    console.log(value);
+app.post('/getGroup', function(req, res) {
     Table = "GROUP";
-    group_id = "" + value
 
     var params = {
         TableName : Table,
-        Key: {
-            "group_id": group_id,
+        ProjectionExpression: "#id, player_id",
+        FilterExpression:
+            "#id = :group",
+        ExpressionAttributeNames: {
+            "#id": "group_id",
+        },
+        ExpressionAttributeValues: {
+            ":group" : req.body.group_id,
         }
     };
 
-    docClient.get(params, function(err, data) {
+    docClient.scan(params, onScan);
+
+    function onScan(err, data) {
         if (err) {
-            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+            console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
         } else {
-            console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+            // print all the movies
+            console.log("Scan succeeded.");
+            data.Items.forEach(function (group) {
+                console.log("player id = " + group.player_id);
+            });
             res.send(data);
+
+            // continue scanning if we have more movies, because
+            // scan can retrieve a maximum of 1MB of data
+            if (typeof data.LastEvaluatedKey != "undefined") {
+                console.log("Scanning for more...");
+                params.ExclusiveStartKey = data.LastEvaluatedKey;
+                docClient.scan(params, onScan);
+            }
         }
-    });
+    }
 });
 
-app.get('/getApplication/:player_id', function(req, res) {
-    value = req.params.player_id;
-    console.log(value);
-    Table = "APPLICATION";
-    player_id = "" + value
+app.post('/getApplication', function(req, res) {
+    Table = "GROUP";
 
     var params = {
         TableName : Table,
-        Key: {
-            "player_id": player_id,
+        ProjectionExpression: "#id",
+        FilterExpression:
+            "#id = :player",
+        ExpressionAttributeNames: {
+            "#id": "player_id",
+        },
+        ExpressionAttributeValues: {
+            ":player" : reg.body.player_id,
         }
     };
 
-    docClient.get(params, function(err, data) {
+    docClient.scan(params, onScan);
+
+    function onScan(err, data) {
         if (err) {
-            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+            console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
         } else {
-            console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+            // print all the movies
+            console.log("Scan succeeded.");
+            data.Items.forEach(function (application) {
+                console.log("player id = " + application.player_id);
+            });
             res.send(data);
+
+            // continue scanning if we have more movies, because
+            // scan can retrieve a maximum of 1MB of data
+            if (typeof data.LastEvaluatedKey != "undefined") {
+                console.log("Scanning for more...");
+                params.ExclusiveStartKey = data.LastEvaluatedKey;
+                docClient.scan(params, onScan);
+            }
         }
-    });
+    }
 });
 
 // catch 404 and forward to error handler
